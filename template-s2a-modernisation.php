106f8e56a9bb5a3b50fd993bc97b56d43ed84a54
Jerome Lachaud <jerome.lachaud@gmail.com>
2014-09-03 17:13:23 +0200

    add new heading pages

---
+++ template-s2a-modernisation.php
@@ -0,0 +1,39 @@
+<?php /* Template Name: S2A-Modernisation */ get_header(); ?>
+
+<?php get_header(); ?>
+
+			<main role="main">
+				<!-- section -->
+				<section>
+
+					<!-- Webflow Content Block -->
+					<div class="content-block">
+						<h1><?php _e( '', 'html5blank' ); single_cat_title(); ?></h1>
+					</div>
+					<!-- / Webflow Content Block -->
+
+					<!-- Webflow Content Block -->
+					<div class="content-block">
+							<?php get_template_part('loop-modernisation-liste'); ?>
+					</div>
+					<!-- / Webflow Content Block -->
+
+					<!-- Webflow Content Block -->
+					<div class="content-block">
+						<?php get_template_part('pagination'); ?>
+					</div>
+					<!-- / Webflow Content Block -->
+
+				</section>
+				<!-- /section -->
+			</main>
+
+			<!-- Webflow Flooter -->
+			<div class="footer-section">
+				<?php get_footer(); ?>
+			</div>
+
+	<!-- /Webflow Righ Col -->
+	</div>
+
+<?php get_sidebar(); ?>
